fe890ad34305df46a5a2a55030f801dc49a83140
TRASJEPS <71234985+TRASJEPS@users.noreply.github.com>
2021-09-06 12:12:58 -0700

    update 2

---
+++ 01-java-fundamentals/02-alfred-bot/AlfredQuotes.java
@@ -0,0 +1,13 @@
+// TRAVIS JEPSON SEPT 2021
+// ALFREDBOT
+import java.util.Date;
+
+// This only HOLDS all the quote methods
+public class AlfredQuotes 
+{
+    //No need for code here, this is just a method that returns the following text.
+    public String basicGreeting()
+    {
+        return "Hello, its lovely to see you.  How are you today?"
+    }
+}
