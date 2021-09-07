69d8e0d951fdaf37214a03bdf927dd0d11511b97
TRASJEPS <71234985+TRASJEPS@users.noreply.github.com>
2021-09-06 17:35:01 -0700

    update 6 alfred working

--- 01-java-fundamentals/02-alfred-bot/AlfredBotTest.java
+++ 01-java-fundamentals/02-alfred-bot/AlfredBotTest.java
@@ -7,15 +7,22 @@ public class AlfredBotTest
     {
         // This makes an instance that allows you to access all the methods in AlfredQuotes.java
         // the instance here being alfredBot
+        // IS THIS CORRECT
         AlfredQuotes alfredBot = new AlfredQuotes();
 
-        public String alfredBot.basicGreeting();
-        public String alfredBot.guestGreeting("Bobby Ray", "evening");
-        public String alfredBot.dateAnnouncement(11, 13, 2021);
-        public String alfredBot.respondBeforeAlexis("Is Alexis the best ever?");
-        public String alfredBot.respondBeforeAlexis("Where is Alfred? I need a question answered.");
-        public String alfredBot.respondBeforeAlexis("Long ago in a time before I spent money on something that did not have the solution as advertised, I was a bit peeved.");
+        String greetingTest = alfredBot.basicGreeting();
+        String nameTest = alfredBot.guestGreeting("Bobby Ray", "evening");
+        String setTheDateTest = alfredBot.dateAnnouncement(11, 13, 2021);
+        String stringWithAlexisTest = alfredBot.respondBeforeAlexis("Is Alexis the best ever?");
+        String stringWithAlfredTest = alfredBot.respondBeforeAlexis("Where is Alfred? I need a question answered.");
+        String stringWithNothingTest = alfredBot.respondBeforeAlexis("Long ago in a time before I spent money on something that did not have the solution as advertised, I was a bit peeved.");
 
-
+        System.out.println(greetingTest);
+        System.out.println(greetingTest);
+        System.out.println(nameTest );
+        System.out.println(setTheDateTest);
+        System.out.println(stringWithAlexisTest);
+        System.out.println(stringWithAlfredTest);
+        System.out.println(stringWithNothingTest);
     }
 }
--- 01-java-fundamentals/02-alfred-bot/AlfredQuotes.java
+++ 01-java-fundamentals/02-alfred-bot/AlfredQuotes.java
@@ -15,23 +15,23 @@ public class AlfredQuotes
     //Not sure if parameters taken in need to be redefined 
     public String guestGreeting(String name, String dayPeriod)
     {
-        return "Good " + dayPeriod + " " + name ".  How are you today?";
+        return "Good " + dayPeriod + " " + name + ".  How are you today?";
     }
 
     // is this correct?
     // NOT FOLLOWING THE util.DATE, just using the method
-    public String dateAnnouncement(Int month, Int day, Int year)
+    public String dateAnnouncement(int month, int day, int year)
     {
-        return "Greetings! Today's date is " + month + "/" + day + "/" + year ".";
+        return "Greetings! Today's date is " + month + "/" + day + "/" + year + ".";
     }
 
     public String respondBeforeAlexis(String conversation)
     {
-        if(a = conversation.indexOf("Alfred") > 0)
+        if(conversation.indexOf("Alfred") > 0)
         {
             return "At your service, allow me to assist you.";
         }
-        else if(a = conversation.indexOf("Alexis") > 0)
+        else if(conversation.indexOf("Alexis") > 0)
         {
             return "Eww. Alexis is gross. Allow a real AI service to solve your problem.";
         }
