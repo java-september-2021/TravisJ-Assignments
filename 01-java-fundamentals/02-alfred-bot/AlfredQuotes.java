487c8506aacf2534038e16114d166e5424c26387
TRASJEPS <71234985+TRASJEPS@users.noreply.github.com>
2021-09-06 17:08:08 -0700

    update 4

--- 01-java-fundamentals/02-alfred-bot/AlfredQuotes.java
+++ 01-java-fundamentals/02-alfred-bot/AlfredQuotes.java
@@ -8,39 +8,35 @@ public class AlfredQuotes
     //No need for code here, this is just a method that returns the following text.
     public String basicGreeting()
     {
-        return "Hello there, its lovely to see you.  How are you today?"
+        return "Hello there, its lovely to see you.  How are you today?";
     }
 
     //Assuming dayPeriod comes from util.Date...
     //Not sure if parameters taken in need to be redefined 
     public String guestGreeting(String name, String dayPeriod)
     {
-        return "Good " + dayPeriod + " " + name ".  How are you today?"
+        return "Good " + dayPeriod + " " + name ".  How are you today?";
     }
 
-    public String dateAnnouncement()
+    // is this correct?
+    public String dateAnnouncement(Int month, Int day, Int year)
     {
-        return "Today's date is " + date
+        return "Greetings! Today's date is " + month + "/" + day + "/" + year ".";
     }
 
     public String respondBeforeAlexis(String conversation)
     {
-        return "Today's date is " + date
+        if(a = conversation.indexOf("Alfred") > 0)
+        {
+            return "At your service, allow me to assist you.";
+        }
+        else if(a = conversation.indexOf("Alexis") > 0)
+        {
+            return "Eww. Alexis is gross. Allow a real AI service to solve your problem.";
+        }
+        else
+        {
+            return "Indeed. With that, I will sign off for the day.";
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
