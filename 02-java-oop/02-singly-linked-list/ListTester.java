03a1e8a09fcf54c7ca6158ea26bf9ff31ff9aec2
TRASJEPS <71234985+TRASJEPS@users.noreply.github.com>
2021-09-10 17:13:44 -0700

    update 23

---
+++ 02-java-oop/02-singly-linked-list/ListTester.java
@@ -0,0 +1,76 @@
+// TRAVIS JEPSON SEPT 2021
+// OBJECT PLAY
+// Lecture 9/7/21
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PizzaTester 
+{
+
+    // ALL THE METHODS ARE HERE!!
+    public static void main(String[] args)
+    {
+        // This makes an instance that allows you to access all the methods in AlfredQuotes.java
+        // the instance here being BasicsInstance
+        //
+
+// QUESTION - what is a new type called?  an INSTANCE??
+
+        //OLD WAY WHEN public
+        // Pizza cheese = new Pizza();
+        // cheese.name = "Cheese Pizza";
+        //     String[] cheeze = {"Cheese","Sauce"};
+        //     cheese.toppings = cheeze;
+        // cheese.size = 'S';
+        // cheese.slices = 10;
+        // cheese.type = "Pan";
+
+        //NEW  WAY WHEN private
+        Pizza cheese = new Pizza();
+        cheese.setName("Cheese Pizza");
+            String[] cheezeArray = {"Cheese","Sauce"};
+            cheese.setToppings(cheezeArray);
+        cheese.setSize('S'); 
+        cheese.setSlices(10);
+        cheese.setType("Pan");
+
+
+        // cheese.toppings.add("Pepper Jack");
+
+        // IS THIS DEAD SINCE THE CONSTRUCTOR IS NOW SET? ^
+
+        // Because thesea private you now use getName, etc etc
+        // Make sure when using the getName to ADD two parenthesees
+        System.out.println("Pizza Name: " + cheese.getName());
+        System.out.println("Pizza Size: " + cheese.getSize());
+        System.out.println("Pizza Toppings: " + Arrays.toString(cheese.getToppings()));
+
+        // ADD THE ARRAY WITHT HE SQUARE BRACKETS
+        // Pizza meatLovers = new Pizza("Meat Lovers Pizza", String[] meatToppings {"Sausage","Bacon","Ham"}, 'L', "New York");
+
+        String[] meatToppings = {"Sausage","Bacon","Ham"};  // ADD THE TOPPINGS LIKE THIS
+        Pizza meatLovers = new Pizza("Meat Lovers Pizza", meatToppings, 'L', "New York");
+
+        String[] veggieToppings = {"Grass","Flowers","Lettice","Leaves"};  
+        Pizza veggiePizza = new Pizza("Veggie", veggieToppings, 'S', "Thin Crust");
+
+        // String[] spinachToppings = {"Grass","Flowers","Lettice","Leaves"};  // DONT NEED ANYMORE
+        // NOW YOU CAN MAKE THE ARRAY IN ONE LINE!
+        Pizza spinachSpecial = new Pizza("Spinach", new String[] {"Spinach","More Spinach","Cheese","Olives"}, 'L', "Thick Crust", 12);
+        
+        // THIS WILL NOT WORK BASED ON CURRENT constructor
+        // Pizza spinachSpecial = new Pizza(new String[] {"Spinach","More Spinach","Cheese","Olives"}, "Spinach", 'L', "Thick Crust");
+
+        System.out.print("Pizza Name: " + meatLovers.getName() + "   ");
+        System.out.print("Pizza Size: " + meatLovers.getSize() + "   ");
+        System.out.println("Pizza Toppings: " + Arrays.toString(meatLovers.getToppings())+ "\n");
+
+        System.out.println("Pizza Name: " + veggiePizza.getName());
+        System.out.println("Pizza Size: " + veggiePizza.getSize());
+        System.out.println("Pizza Toppings: " + Arrays.toString(veggiePizza.getToppings())+ "\n");
+
+        //CHECK TOTAL PIZZAS MADE        
+        System.out.println("Total Pizzas Made: " + Pizza.getNumberOfPizzas()+ "\n");
+        System.out.println(Pizza.advertise());
+    }
+}
---
+++ 02-java-oop/02-singly-linked-list/SinglyLinkedList.java
@@ -0,0 +1,160 @@
+// TRAVIS JEPSON SEPT 2021
+// OBJECT PLAY
+// Lecture 9/7/21
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// This is a METHOD
+public class SinglyLinked 
+{
+    //attributes
+    // CHANGE TO PRIVATE SHORTLY??
+    // SO GETTERS AND SETTERS ARE HOW YOU ACCESS THEM
+        private String name;
+        private String[] toppings;
+        // public String[] toppings = new String[] = {"Cheese","Sauce"}; // sets default ASK IF CORRECT
+        // public ArrayList<String> toppings;  // MORE DYNAMIC SEE LINE 20
+        private char size;
+        private int slices;
+        private String type;
+
+        // ALL OF THESE ATTRIBUTES ARE EMPTY BUT THEIR TYPE IS SOLID
+        // all of these can be assigned a default value, THESE ARE OVERRIDDEN BY THE CONSTRUCTOR(s)
+
+        // keep track of all pizzas sold
+        // if this is PRIVATE
+        // THIS IS STATIC BECAUSE IT IS BELONGING OT THE CLASS RATHER THAN THE OBJECT 
+        // keep track of more general attributes
+        static int numberOfPizzas;
+        
+        // THIS IS FOR cheese empty default pizza 26-33
+        //NOTHING HERE - THIS IS THE BEAN!
+        public Pizza()
+        {
+            numberOfPizzas++;
+        }
+        // MAKE SURE THIS IS ACCURATELY CALLED ETC
+        // its this.ATTRIBuTE because it links in the given situation?
+
+        //THIS METHOD IS CLASS SPECIFIC
+        // AKA A constructor type default
+        // public is the access modifier
+        // THIS IS FOR THE SPECIFIC CONSTRUCTOR
+        public Pizza(String name, String[] toppings, char size, String type, int slices)
+        {
+            this.name = name;
+            this.toppings = toppings;
+            this.size = size;
+            this.type = type;
+            this.slices = slices;
+            numberOfPizzas++;
+        }
+        //THIS METHOD IS CLASS SPECIFIC
+    // public is the access modifier
+    // removed TOPPINGS AREA for this constructor
+    // this will take over for DEFAUL
+    public Pizza(String name, char size, String type)
+    {
+        this.name = name;
+        this.toppings = new String[] {"Cheese", "Pepperoni"};
+        this.size = size;
+        this.type = type;
+        this.slices = 10;
+        numberOfPizzas++;
+    }
+
+    public Pizza(String name, char size, String type)
+    {
+        this.name = name;
+        this.toppings = new String[] {"Cheese", "Pepperoni"};
+        this.size = size;
+        this.type = type;
+        this.slices = 10;
+        numberOfPizzas++;
+    }
+
+
+    //setters! WATCH LECTURE
+    // SETTERS ARE VOID
+    public void setName(String name)
+    {
+        if(name.equals(""))
+        {
+            System.out.println("You must enter a name for your Pizza.");
+            return;
+        }
+        this.name = name;
+    }
+    public void setToppings(String[] toppings)
+    {
+        this.toppings = toppings;
+    }
+    public void setSize(char size)
+    {
+        this.size = size;
+    }
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+    public void setSlices(int slices )
+    {
+        this.slices = slices;
+    }
+    // public void setNumberOfPizzas(int totalPizzas )
+    // {
+    //     this.totalPizzas = totalPizzas;
+    // }
+
+    //getters
+     public String getName()
+     {
+         return this.name;
+     }
+     public String[] getToppings()
+     {
+        return this.toppings;
+     }
+     public char getSize()
+     {
+         return this.size;
+     }
+     public String getType()
+     {
+        return this.type;
+     }
+     public int getSlices()
+     {
+         return this.slices;
+     }
+     // ADDED STATIC
+     public static int getNumberOfPizzas()
+    {
+        // this.numberOfPizzas;
+        return numberOfPizzas;
+    }
+
+    
+    public static String advertise()
+    {
+        return "EYYYY we got some Pizza ova here!";
+    }
+
+    public String displayPizza()
+    {
+        return String.format("This is a %s with %s toppings %d slices", this.name, 
+        Arrays.toString(this.toppings), this.slices);
+    }
+
+// THIS METHOD OBJECTS AFFECTS OTHER OBJECTS ATTRIBUTES
+
+    public void pizzaFight(Pizza otherPizza)
+    {
+        int numOfSlices = otherPizza.getSlices();
+        numOfSlices -= 1;
+        otherPizza.setSlices(numOfSlices);
+
+        System.out.println(this.name + " just pwnt " + otherPizza.getName() + " and " + otherPizza.getName() + " currently has " + otherPizza.getSlices());
+    }
+}
