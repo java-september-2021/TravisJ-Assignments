ee71443c218a9bb10cd744cafe1775d288c9f794
TRASJEPS <71234985+TRASJEPS@users.noreply.github.com>
2021-10-27 20:42:29 -0700

    update 84

---
+++ 03-java-spring/02-spring-data-i/03-languages/WORKING asc and dec query/albums/src/main/java/com/travis/jepson/java/tests/AlbumsApplication.java
@@ -0,0 +1,13 @@
+package com.travis.jepson.java.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumsApplication.class, args);
+	}
+
+}
